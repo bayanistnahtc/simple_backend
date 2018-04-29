5a77ce8eaa35d57342c283b8153d0754fa895df0
bayanistnahtc <bayanistnahtc@gmail.com>
2018-04-29 16:35:05 +0300

    updated files access

--- mhelp/views.py
+++ mhelp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 import os
 from django.conf import settings
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, Http404
 
 def download(request, file):
     file_path = "static/" + file
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-word")
-            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
-    raise Http404
+    # raise Http404
 
